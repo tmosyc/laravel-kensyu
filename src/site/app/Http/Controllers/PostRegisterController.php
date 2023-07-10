9a4dbfaa0aa99a2cbcde5edf8b96f9f24b8a3b55
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-07-10 10:57:05 +0900

    会員登録機能作成

---
+++ src/site/app/Http/Controllers/PostRegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class PostRegisterController
+{
+    public static function registerNextView()
+    {
+        self::registerUserInfo(request());
+        return redirect('/posts');
+    }
+
+    public static function registerUserInfo(Request $request)
+    {
+        $name = $request->input('register_name');
+        $email = $request->input('register_email');
+        $password = $request->input('register_password');
+        $password_hash = Hash::make($password);
+
+        $insert_user = [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password_hash
+        ];
+        DB::table('users')->insert($insert_user);
+        Session::put('name',$name);
+        Session::put('email',$email);
+    }
+}
---
+++ src/site/app/Http/Controllers/RegisterPageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+class RegisterPageController
+{
+    public static function registerPageView()
+    {
+        return view('register');
+    }
+}
