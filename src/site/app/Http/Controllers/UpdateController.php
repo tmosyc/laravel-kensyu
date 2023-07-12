1598bfc59200253296a78e99d7f1ccad44bec286
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-07-12 20:24:24 +0900

    update機能の実装

--- src/site/app/Http/Controllers/UpdateController.php
+++ src/site/app/Http/Controllers/UpdateController.php
@@ -1,6 +1,22 @@
-<?php
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
 
 class UpdateController
 {
-
+    public static function updateView($number)
+    {
+        return view('update',['article_id'=>$number]);
+    }
+    public static function updateData($number,Request $request)
+    {
+        Article::where('article_id',$number)->update([
+            'title'=>$request->input('update_title'),
+            'content' => $request->input('update_content')
+        ]);
+        return redirect('/posts/'.$number);
+    }
 }
