8683dc8c15c0f9ac82a8194afc488fc3e43d5bba
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-07-18 15:15:02 +0900

    登録機能におけるテストの追加

--- src/site/tests/Unit/RegisterTest.php
+++ src/site/tests/Unit/RegisterTest.php
@@ -3,19 +3,23 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use App\Repo\UserRegisterRepo;
 use Faker\Factory;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
-class RegisterTest extends TestCase
+class registerTest extends TestCase
 {
     use RefreshDatabase;
     use WithFaker;
+    use WithoutMiddleware;
 
     public function test_登録情報のリクエストが送られた時にDBに登録されるか確認すること()
     {
+        $this->withoutMiddleware();
 
         User::factory()->create();
         $user_register_mock = [
@@ -26,10 +30,28 @@ class RegisterTest extends TestCase
 
         $res = $this->post('/register',$user_register_mock);
 
-        $res -> assertStatus(302);
+        $res->assertStatus(302);
         $this -> assertDatabaseHas('users',[
             'email'=>$user_register_mock['register_email']
             ]
         );
+    }
+
+    public function test_セッションに登録するための関数が動いていること()
+    {
+        $email = 'mike@example.com';
+        User::factory()->create([
+            'name'=>'mike',
+            'email'=>$email,
+            'password'=>'mike5555'
+        ]);
+
+        $user = User::where('email', $email)->first();
+
+        $user_id = UserRegisterRepo::sessionId($user->email);
+
+        $maxId = User::max('id');
+
+        self::assertSame($user_id,$maxId);
     }
 }
